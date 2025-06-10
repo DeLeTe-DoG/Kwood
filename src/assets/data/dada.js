525f9a73a9771a4468634157eb8218b309bda1d1
Asia <asiamadaminova@yandex.ru>
2025-06-10 23:22:59 +0300

    new data added

---
+++ src/assets/data/dada.js
@@ -0,0 +1,57 @@
+import heart_box from '../images/goods-photos/heart-box.png'
+
+import twin_rings_box from '../images/goods-photos/twin-rings-box.png'
+
+import ring_heart_box from '../images/goods-photos/ring-heart-box.png'
+
+import faceted_ring from '../images/goods-photos/faceted-ring.jpg'
+
+// import  from '../images/goods-photos/ring-heart-box.png'
+// import ring_heart_box from '../images/goods-photos/ring-heart-box.png'
+
+const shop_data = [
+    {
+        id: 1,
+        name: 'Подвеска "Сердце"',
+        cost: 600,
+        material: 'Амарант',
+        img: heart_box,
+    },
+    {
+        id: 2,
+        name: 'Кольца парные',
+        cost: 1000,
+        material: 'Дуб',
+        img: twin_rings_box,
+    },
+    {
+        id: 3,
+        name: 'Кольцо "Сердце"',
+        cost: 500,
+        material: 'Амарант',
+        img: ring_heart_box,
+    },
+    {
+        id: 4,
+        name: 'Кольцо "Айсберг"',
+        cost: 500,
+        material: 'Клен',
+        img: faceted_ring,
+    },
+    // {
+    //     id: 5,
+    //     name: 'Кольцо "Сердце"',
+    //     cost: 500,
+    //     material: 'Амарант',
+    //     img: ring_heart_box,
+    // },
+    // {
+    //     id: 6,
+    //     name: 'Кольцо "Сердце"',
+    //     cost: 500,
+    //     material: 'Амарант',
+    //     img: ring_heart_box,
+    // },
+]
+
+export default shop_data
